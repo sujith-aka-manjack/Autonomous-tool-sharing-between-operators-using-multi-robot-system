02c0dc1ebc27e1e61790b677dc4208931a753755
Genki Miyauchi <4963919-genki_miyauchi@users.noreply.gitlab.com>
2021-11-11 13:06:56 +0000

    Add outline to leader

--- src/web_client/js/entities/EpuckLeader.js
+++ src/web_client/js/entities/EpuckLeader.js
@@ -24,7 +24,7 @@ class EpuckLeader {
     geometry.rotateX(-1.572);
 
     /* Bring to on top of zero*/
-    geometry.translate(0, 0, 53 * UNIT_SCALE * 0.5 + 4.7 * UNIT_SCALE);
+    geometry.translate(0, 0, 86 * UNIT_SCALE * 0.5);
 
     var material = new THREE.MeshPhongMaterial({
       color: 0x2fffff
@@ -35,6 +35,15 @@ class EpuckLeader {
     var meshParent = new THREE.Group();
     /* Add all parts to a parent mesh */
     meshParent.add(epuck_leader);
+
+      /* Outline */
+    var outlineMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.BackSide } );
+    var outlineGeometry = geometry.clone();
+    outlineGeometry.translate(0, 0, -(86 * UNIT_SCALE * 0.5)); // Temporarily return to original position
+    var outlineMesh = new THREE.Mesh( outlineGeometry, outlineMaterial );
+    outlineMesh.scale.multiplyScalar(1.1);
+    outlineMesh.position.set(0, 0, 86 * UNIT_SCALE * 0.5); // Reassign its position above the surface
+    meshParent.add( outlineMesh );
 
     // /* LEDs */
     // for (let i = 0; i < 12; ++i) {
@@ -53,31 +62,31 @@ class EpuckLeader {
     //   meshParent.add(led);
     // }
 
-    /* Add Intersection Points */
-    var pointsGeom = new THREE.BufferGeometry();
-    pointsGeom.setAttribute('position', new THREE.BufferAttribute(
-      new Float32Array(24 * 3), // 24 points * 3 axis per point
-      3
-    ));
+    // /* Add Intersection Points */
+    // var pointsGeom = new THREE.BufferGeometry();
+    // pointsGeom.setAttribute('position', new THREE.BufferAttribute(
+    //   new Float32Array(24 * 3), // 24 points * 3 axis per point
+    //   3
+    // ));
 
-    var points = new THREE.Points(pointsGeom, new THREE.PointsMaterial({
-      color: 0x000000
-    }));
-    meshParent.add(points);
+    // var points = new THREE.Points(pointsGeom, new THREE.PointsMaterial({
+    //   color: 0x000000
+    // }));
+    // meshParent.add(points);
 
-    /* Add lines for rays */
-    for (let i = 0; i < 24; i++) {
-      var lineGeom = new THREE.BufferGeometry();
+    // /* Add lines for rays */
+    // for (let i = 0; i < 24; i++) {
+    //   var lineGeom = new THREE.BufferGeometry();
 
-      // attributes
-      var linesPos = new Float32Array(2 * 3); //2 points per line * 3 axis per point
-      lineGeom.setAttribute('position', new THREE.BufferAttribute(linesPos, 3));
+    //   // attributes
+    //   var linesPos = new Float32Array(2 * 3); //2 points per line * 3 axis per point
+    //   lineGeom.setAttribute('position', new THREE.BufferAttribute(linesPos, 3));
 
-      var line = new THREE.Line(lineGeom);
+    //   var line = new THREE.Line(lineGeom);
 
-      meshParent.add(line);
-      that.lines.push(line);
-    }
+    //   meshParent.add(line);
+    //   that.lines.push(line);
+    // }
 
     /* Update mesh parent */
     meshParent.position.x = entity.position.x * scale;
@@ -106,72 +115,72 @@ class EpuckLeader {
         entity.orientation.z,
         entity.orientation.w));
 
-      if (entity.leds) {
-        /* Update LED colors */
-        for (let i = 0; i < 12; i++) {
-          this.mesh.children[1 + i].material.color.setHex(entity.leds[i]);
-          this.mesh.children[1 + i].material.emissive.setHex(entity.leds[i]);
-        }
-      }
+      // if (entity.leds) {
+      //   /* Update LED colors */
+      //   for (let i = 0; i < 12; i++) {
+      //     this.mesh.children[1 + i].material.color.setHex(entity.leds[i]);
+      //     this.mesh.children[1 + i].material.emissive.setHex(entity.leds[i]);
+      //   }
+      // }
 
-      var pointMesh = this.mesh.children[13];
+      // var pointMesh = this.mesh.children[13];
 
-      if (entity.points.length > 0) {
-        var points = pointMesh.geometry.getAttribute('position').array
+      // if (entity.points.length > 0) {
+      //   var points = pointMesh.geometry.getAttribute('position').array
 
-        for (let i = 0; i < entity.points.length; i++) {
-          var pointVals = entity.points[i].split(",")
-          points[3 * i] = pointVals[0] * scale
-          points[3 * i + 1] = pointVals[1] * scale
-          points[3 * i + 2] = pointVals[2] * scale
-        }
-        pointMesh.geometry.getAttribute('position').needsUpdate = true;
-      }
+      //   for (let i = 0; i < entity.points.length; i++) {
+      //     var pointVals = entity.points[i].split(",")
+      //     points[3 * i] = pointVals[0] * scale
+      //     points[3 * i + 1] = pointVals[1] * scale
+      //     points[3 * i + 2] = pointVals[2] * scale
+      //   }
+      //   pointMesh.geometry.getAttribute('position').needsUpdate = true;
+      // }
 
-      /* Only draw given points, and hide all previous points */
-      pointMesh.geometry.setDrawRange(0, entity.points.length);
+      // /* Only draw given points, and hide all previous points */
+      // pointMesh.geometry.setDrawRange(0, entity.points.length);
 
-      if (entity.rays.length > 0) {
-        for (let i = 0; i < entity.rays.length; i++) {
-          /*
-              For each ray as a string,
-              format -> "BoolIsChecked:Vec3StartPoint:Vec3EndPoint"
-              For example -> "true:1,2,3:1,2,4"
-          */
+      // if (entity.rays.length > 0) {
+      //   for (let i = 0; i < entity.rays.length; i++) {
+      //     /*
+      //         For each ray as a string,
+      //         format -> "BoolIsChecked:Vec3StartPoint:Vec3EndPoint"
+      //         For example -> "true:1,2,3:1,2,4"
+      //     */
 
-          var rayArr = entity.rays[i].split(":")
-          var start = rayArr[1].split(",")
-          var end = rayArr[2].split(",")
+      //     var rayArr = entity.rays[i].split(":")
+      //     var start = rayArr[1].split(",")
+      //     var end = rayArr[2].split(",")
 
-          var line = this.mesh.children[14 + i];
-          if (line) {
-            if (rayArr[0] == "true") {
-              line.material.color.setHex(0xff00ff);
-            } else {
-              line.material.color.setHex(0x00ffff);
-            }
+      //     var line = this.mesh.children[14 + i];
+      //     if (line) {
+      //       if (rayArr[0] == "true") {
+      //         line.material.color.setHex(0xff00ff);
+      //       } else {
+      //         line.material.color.setHex(0x00ffff);
+      //       }
 
-            var positions = line.geometry.getAttribute('position').array
+      //       var positions = line.geometry.getAttribute('position').array
 
-            positions[0] = start[0] * scale
-            positions[1] = start[1] * scale
-            positions[2] = start[2] * scale
+      //       positions[0] = start[0] * scale
+      //       positions[1] = start[1] * scale
+      //       positions[2] = start[2] * scale
 
-            positions[3] = end[0] * scale
-            positions[4] = end[1] * scale
-            positions[5] = end[2] * scale
+      //       positions[3] = end[0] * scale
+      //       positions[4] = end[1] * scale
+      //       positions[5] = end[2] * scale
 
-            line.geometry.getAttribute('position').needsUpdate = true;
-            line.geometry.setDrawRange(0, 2);
-          }
-        }
-      }
+      //       line.geometry.getAttribute('position').needsUpdate = true;
+      //       line.geometry.setDrawRange(0, 2);
+      //     }
+      //   }
+      // }
 
-      /* Hide all the previous lines */
-      /* 14 are the number of objects in meshParent before rays */
-      for (let i = 14 + entity.rays.length; i < this.mesh.children.length; i++) {
-        this.mesh.children[i].geometry.setDrawRange(0, 0);
-      }
+      // /* Hide all the previous lines */
+      // /* 14 are the number of objects in meshParent before rays */
+      // for (let i = 14 + entity.rays.length; i < this.mesh.children.length; i++) {
+      //   this.mesh.children[i].geometry.setDrawRange(0, 0);
+      // }
     }
   }
 }
