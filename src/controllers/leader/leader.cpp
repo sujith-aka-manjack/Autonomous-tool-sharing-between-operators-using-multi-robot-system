4332182485e004f41fd07cd72b59943ba86ec05a
Genki Miyauchi <4963919-genki_miyauchi@users.noreply.gitlab.com>
2021-11-24 15:17:42 +0000

    Changed leader teleoperation control

--- src/controllers/leader/leader.cpp
+++ src/controllers/leader/leader.cpp
@@ -1203,45 +1203,21 @@ void CLeader::SetWheelSpeedsFromVector(const CVector2& c_heading) {
     /* Clamp the speed so that it's not greater than MaxSpeed */
     Real fBaseAngularWheelSpeed = Min<Real>(fHeadingLength, m_sWheelTurningParams.MaxSpeed);
 
-    /* Turning state switching conditions */
-    if(Abs(cHeadingAngle) <= m_sWheelTurningParams.NoTurnAngleThreshold) {
-        /* No Turn, heading angle very small */
-        m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::NO_TURN;
-    }
-    else if(Abs(cHeadingAngle) > m_sWheelTurningParams.HardTurnOnAngleThreshold) {
-        /* Hard Turn, heading angle very large */
-        m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::HARD_TURN;
-    }
-    else if(m_sWheelTurningParams.TurningMechanism == SWheelTurningParams::NO_TURN &&
-            Abs(cHeadingAngle) > m_sWheelTurningParams.SoftTurnOnAngleThreshold) {
-        /* Soft Turn, heading angle in between the two cases */
-        m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::SOFT_TURN;
-    }
-
     /* Wheel speeds based on current turning state */
     Real fSpeed1, fSpeed2;
-    switch(m_sWheelTurningParams.TurningMechanism) {
-        case SWheelTurningParams::NO_TURN: {
-            /* Just go straight */
-            fSpeed1 = fBaseAngularWheelSpeed;
-            fSpeed2 = fBaseAngularWheelSpeed;
-            break;
-        }
 
-        case SWheelTurningParams::SOFT_TURN: {
-            /* Both wheels go straight, but one is faster than the other */
-            Real fSpeedFactor = (m_sWheelTurningParams.HardTurnOnAngleThreshold - Abs(cHeadingAngle)) / m_sWheelTurningParams.HardTurnOnAngleThreshold;
-            fSpeed1 = fBaseAngularWheelSpeed - fBaseAngularWheelSpeed * (1.0 - fSpeedFactor);
-            fSpeed2 = fBaseAngularWheelSpeed + fBaseAngularWheelSpeed * (1.0 - fSpeedFactor);
-            break;
-        }
-
-        case SWheelTurningParams::HARD_TURN: {
-            /* Opposite wheel speeds */
-            fSpeed1 = -m_sWheelTurningParams.MaxSpeed;
-            fSpeed2 =  m_sWheelTurningParams.MaxSpeed;
-            break;
-        }
+    if(c_heading.GetX() > 0) {
+        /* Go straight */
+        fSpeed1 = fBaseAngularWheelSpeed;
+        fSpeed2 = fBaseAngularWheelSpeed;
+    } else if(c_heading.GetX() < 0) {
+        /* Go back */
+        fSpeed1 = -fBaseAngularWheelSpeed;
+        fSpeed2 = -fBaseAngularWheelSpeed;
+    } else {
+        /* Rotate */
+        fSpeed1 = -fBaseAngularWheelSpeed;
+        fSpeed2 = fBaseAngularWheelSpeed;
     }
 
     /* Apply the calculated speeds to the appropriate wheels */
