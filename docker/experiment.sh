fd2edf7ebdfdcbbc9fd31556438152393fc32f1b
Genki Miyauchi <4963919-genki_miyauchi@users.noreply.gitlab.com>
2022-06-23 12:02:09 +0100

    Update docker related scripts

--- docker/experiment.sh
+++ docker/experiment.sh
@@ -18,12 +18,12 @@ echo "Mode: $mode"
 if [ $mode = "run" ]
 then
     echo "Starting docker container..."
-    cd ~/Documents
+    cd ~/Documents/experiment
     docker run --name argos -d -p 3000:3000 -p 8000:8000 -p 5000:5000 -v $(pwd)/results:/home/docker/argos-sct/results -w /home/docker/argos-sct genki15/argos-sct python3 src/web_app/app.py
 elif [ $mode = "compress" ]
 then
     echo "Compressing experiment data..."
-    cd ~/Documents
+    cd ~/Documents/experiment
     filename=$(date +"results_%Y-%m-%d_%H:%M")
     zip -r ./$filename.zip results/*
     mv results $filename
