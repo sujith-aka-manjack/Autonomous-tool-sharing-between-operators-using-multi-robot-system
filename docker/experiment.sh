6ca7d7a1b31b750233f3625b16dff2f1f0433357
Genki Miyauchi <4963919-genki_miyauchi@users.noreply.gitlab.com>
2022-07-08 12:29:27 +0100

    Updated command to make docker use host ip address

--- docker/experiment.sh
+++ docker/experiment.sh
@@ -25,7 +25,8 @@ if [ $mode = "run" ]
 then
     echo "Starting docker container..."
     cd ~/Documents/hsi-experiment
-    docker run --name argos -it -p 3000:3000 -p 8000:8000 -p 5000:5000 -v $(pwd)/results:/home/docker/argos-sct/results -w /home/docker/argos-sct genki15/argos-sct python3 src/web_app/app.py -m $condition -o $order
+    # docker run --name argos -it -p 3000:3000 -p 8000:8000 -p 5000:5000 -v $(pwd)/results:/home/docker/argos-sct/results -w /home/docker/argos-sct genki15/argos-sct python3 src/web_app/app.py -m $condition -o $order
+    docker run --name argos -it --network="host" -v $(pwd)/results:/home/docker/argos-sct/results -w /home/docker/argos-sct genki15/argos-sct python3 src/web_app/app.py -m $condition -o $order
 elif [ $mode = "compress" ]
 then
     echo "Compressing experiment data..."
